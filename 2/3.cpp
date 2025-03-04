0e087039c1ca97fbeda9d2fe74449e06299fafc6
SHM_white <1131287310a@gmail.com>
2025-03-04 21:03:25 +0800

    4

---
+++ 2/3.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+class RoomInfo{
+public:
+    bool setId(const std::string& str){
+        strcpy(id, str.c_str());
+        return true;
+    }
+    bool setPrice(int i){ 
+        if(i < 0 || i > 10000){
+            std::cout << "Error: illegal price" << std::endl;
+            return false;
+        }
+        price = i; 
+        return true;
+    }
+    bool setBooked(bool b){ booked = b; return true; }
+    bool setCname(const std::string& str){ strcpy(cname, str.c_str()); return true; }
+    bool setCid(const std::string& str){ strcpy(cid, str.c_str()); return true; }
+    char id[10];
+    int  price;
+    bool booked;
+    char cname[20];
+    char cid[20];
+};
+std::istream& operator>>(std::istream& is, RoomInfo& room){
+    std::string id, cname, cid;
+    int price;
+    bool booked;
+    is >> id >> price >> booked >> cname >> cid;
+    room.setId(id) && room.setPrice(price) && room.setBooked(booked) && room.setCname(cname) && room.setCid(cid);
+    return is;
+}
+std::ostream& operator<<(std::ostream& os, RoomInfo& room){
+    os << room.id << " " << room.price << " " << room.booked << " " << std::ios::boolalpha << room.cname << " " << room.cid;
+    return os;
+}
+bool SaveRooms(std::vector<RoomInfo>& rooms){
+    std::fstream out{"22.bin", std::ios::out | std::ios::binary};
+    if(!out.is_open()){ return false; }
+    for(auto& i : rooms){
+        out.write(reinterpret_cast<char*>(&i), sizeof(RoomInfo));
+    }
+    return true;
+}
+bool AddRoomInfo(std::vector<RoomInfo>& rooms){
+    RoomInfo newInfo;
+    std::cout << "id|price|booked(bool)|cname|cid" << std::endl;
+    std::cin >> newInfo;
+    rooms.push_back(newInfo);
+    return SaveRooms(rooms);
+}
+void ShowAllRoomInfos(std::vector<RoomInfo>& rooms, bool ask = false){
+    if(ask){
+        std::cout << "Show all room infos?(y/n)";
+        char in;
+        std::cin >> in;
+        if(std::tolower(in) != 'y'){
+            return;
+        }
+    }
+    std::cout << "index|id|price|booked(bool)|cname|cid" << std::endl;
+    for (size_t i = 0; i < rooms.size(); i++)
+    {
+        std::cout << i << " " << rooms[i] << std::endl;
+    }
+}
+bool DeleteRoomInfo(std::vector<RoomInfo>& rooms){
+    ShowAllRoomInfos(rooms, true);
+    std::cout << "Index:";
+    int index;
+    std::cin >> index;
+    rooms.erase(rooms.begin() + index);
+    return SaveRooms(rooms);
+}
+int main(){
+    std::vector<RoomInfo> rooms;
+    std::ifstream input{"22.bin", std::ios::binary | std::ios::in};
+    while (!input.eof())
+    {
+        RoomInfo ri;
+        input.read(reinterpret_cast<char*>(&ri), sizeof(RoomInfo));
+        rooms.push_back(std::move(ri));
+    }
+    input.close();
+    char command;
+    while (1)
+    {
+        std::cin >> command;
+        switch (command)
+        {
+        case 'a':
+            
+            break;
+        
+        default:
+            break;
+        }
+    }
+    
+}
